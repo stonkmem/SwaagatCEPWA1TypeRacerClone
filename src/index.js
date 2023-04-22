3e6cb86961b39319fe9dbe853c073ca408f55b9f
stonkmem <73430589+stonkmem@users.noreply.github.com>
2023-04-23 03:12:37 +0800

    Finalise index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import {App} from './app';
 const root = ReactDOM.createRoot(document.getElementById('quote'));
-document.addEventListener("keypress", App);
 root.render(<App/>);
